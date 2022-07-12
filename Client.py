710c715777fc26c2ae8cef267d43e4eb0e58b6b8
WispySparks <wispysparks@gmail.com>
2022-07-11 20:41:00 -0500

    clean up

--- Client.py
+++ Client.py
@@ -1,20 +1,19 @@
 import socket
 privateKey: str = "?!(asLK03,,yB/["
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Socket Created")
 sock.settimeout(15)
-ip = socket.gethostbyname("A-COMPUTER")
-# ip = "192.168.0.18"
-port = 17340
+ip: str = "localhost"
+port: int = 17340
 print("Attempting Connection at " + ip + ", Port: " + str(port))
-connection = sock.connect_ex((ip, port))
+connection: int = sock.connect_ex((ip, port))
 if (connection == 0):
     print("Connected")
     while True:
         try:
             msgBuffer = sock.recv(4096)
             if (msgBuffer.decode() == "Command"):
-                command = input("Input Command: ").strip()
+                command: str = input("Input Command: ").strip()
                 sock.send((privateKey + " " + command).encode())
             elif (msgBuffer):
                 print(msgBuffer.decode())
